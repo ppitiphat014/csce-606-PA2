3576805c62ef53fc377e7094f5eda3e2b2262db8
Praewa Pitiphat <riolu_san@tamu.edu>
2025-02-09 15:07:43 -0600

    finished implementing all CRUD fucntions with some styles for all pages

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,10 +1,89 @@
+# class MoviesController < ApplicationController
+#  before_action :set_movie, only: %i[ show edit update destroy ]
+#
+#  # GET /movies or /movies.json
+#  def index
+#    @movies = Movie.all
+#    @column_names = Movie.column_names - [ "id", "description", "created_at", "updated_at" ]
+#  end
+#
+#  # GET /movies/1 or /movies/1.json
+#  def show
+#  end
+#
+#  # GET /movies/new
+#  def new
+#    @movie = Movie.new
+#  end
+#
+#  # GET /movies/1/edit
+#  def edit
+#  end
+#
+#  # POST /movies or /movies.json
+#  def create
+#    @movie = Movie.new(movie_params)
+#
+#    respond_to do |format|
+#      if @movie.save
+#        format.html { redirect_to @movie, notice: "Movie was successfully created." }
+#        format.json { render :show, status: :created, location: @movie }
+#      else
+#        format.html { render :new, status: :unprocessable_entity }
+#        format.json { render json: @movie.errors, status: :unprocessable_entity }
+#      end
+#    end
+#  end
+#
+#  # PATCH/PUT /movies/1 or /movies/1.json
+#  def update
+#    respond_to do |format|
+#      if @movie.update(movie_params)
+#        format.html { redirect_to @movie, notice: "Movie was successfully updated." }
+#        format.json { render :show, status: :ok, location: @movie }
+#      else
+#        format.html { render :edit, status: :unprocessable_entity }
+#        format.json { render json: @movie.errors, status: :unprocessable_entity }
+#      end
+#    end
+#  end
+#
+#  # DELETE /movies/1 or /movies/1.json
+#  def destroy
+#    @movie.destroy!
+#
+#    respond_to do |format|
+#      format.html { redirect_to movies_path, status: :see_other, notice: "Movie was successfully destroyed." }
+#      format.json { head :no_content }
+#    end
+#  end
+#
+#  private
+#    # Use callbacks to share common setup or constraints between actions.
+#    def set_movie
+#      @movie = Movie.find(params.expect(:id))
+#    end
+#
+#    # Only allow a list of trusted parameters through.
+#    def movie_params
+#      params.expect(movie: [ :title, :rating, :description, :release_date ])
+#    end
+# end
+#
+#
 class MoviesController < ApplicationController
-  before_action :set_movie, only: %i[ show edit update destroy ]
+  before_action :set_movie, only: %i[show edit update destroy]
+
+  # The method helper_method is to explicitly share some methods defined in the controller to make them available for the view.
+  helper_method :sort_column, :sort_direction
 
   # GET /movies or /movies.json
   def index
-    @movies = Movie.all
-    @column_names = Movie.column_names - [ "id", "description", "created_at", "updated_at" ]
+    session[:sort] = params[:sort] if params[:sort]
+    session[:direction] = params[:direction] if params[:direction]
+
+    @movies = Movie.order("#{sort_column} #{sort_direction}")
+    @column_names = [ "title", "rating", "release_date" ]
   end
 
   # GET /movies/1 or /movies/1.json
@@ -59,13 +138,23 @@ class MoviesController < ApplicationController
   end
 
   private
+
     # Use callbacks to share common setup or constraints between actions.
     def set_movie
-      @movie = Movie.find(params.expect(:id))
+      @movie = Movie.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
     def movie_params
-      params.expect(movie: [ :title, :rating, :description, :release_date ])
+      params.require(:movie).permit(:title, :rating, :description, :release_date)
+    end
+
+    # Methods to handle sorting
+    def sort_column
+      %w[title rating release_date].include?(session[:sort]) ? session[:sort] : "title"
+    end
+
+    def sort_direction
+      %w[asc desc].include?(session[:direction]) ? session[:direction] : "asc"
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,32 +12,42 @@
 # Seed the RottenPotatoes DB with some movies.
 more_movies = [
   { title: 'My Neighbor Totoro', rating: 'G',
-    release_date: '16-Apr-1988' },
+    release_date: '16-Apr-1988',
+    description: "This acclaimed animated tale by director Hayao Miyazaki follows schoolgirl Satsuke and her younger sister, Mei, as they settle into an old country house with their father and wait for their mother to recover from an illness in an area hospital. As the sisters explore their new home, they encounter and befriend playful spirits in their house and the nearby forest, most notably the massive cuddly creature known as Totoro." },
   { title: 'Green Book', rating: 'PG-13',
-    release_date: '16-Nov-2018' },
+    release_date: '16-Nov-2018',
+    description: "Dr. Don Shirley is a world-class African-American pianist who's about to embark on a concert tour in the Deep South in 1962. In need of a driver and protection, Shirley recruits Tony Lip, a tough-talking bouncer from an Italian-American neighborhood in the Bronx. Despite their differences, the two men soon develop an unexpected bond while confronting racism and danger in an era of segregation." },
   { title: 'Parasite', rating: 'R',
-    release_date: '30-May-2019' },
+    release_date: '30-May-2019',
+    description: "Greed and class discrimination threaten the newly formed symbiotic relationship between the wealthy Park family and the destitute Kim clan." },
   { title: 'Nomadland', rating: 'R',
-    release_date: '19-Feb-2021' },
+    release_date: '19-Feb-2021',
+    description: "A woman embarks on a journey through the American West after losing everything during the recession." },
   { title: 'CODA', rating: 'PG-13',
-    release_date: '13-Aug-2021' },
+    release_date: '13-Aug-2021',
+    description: "Seventeen-year-old Ruby (Emilia Jones) is the sole hearing member of a deaf family -- a CODA, child of deaf adults. Her life revolves around acting as interpreter for her parents (Marlee Matlin, Troy Kotsur) and working on the family's struggling fishing boat every day before school with her father and older brother (Daniel Durant). But when Ruby joins her high school's choir club, she discovers a gift for singing and soon finds herself drawn to her duet partner Miles (Ferdia Walsh-Peelo). Encouraged by her enthusiastic, tough-love choirmaster (Eugenio Derbez) to apply to a prestigious music school, Ruby finds herself torn between the obligations she feels to her family and the pursuit of her own dreams.
+    Content expanded." },
   { title: 'The Matrix', rating: 'R',
-    release_date: '31-Mar-1999' },
+    release_date: '31-Mar-1999',
+    description: "Neo (Keanu Reeves) believes that Morpheus (Laurence Fishburne), an elusive figure considered to be the most dangerous man alive, can answer his question -- What is the Matrix? Neo is contacted by Trinity (Carrie-Anne Moss), a beautiful stranger who leads him into an underworld where he meets Morpheus. They fight a brutal battle for their lives against a cadre of viciously intelligent secret agents. It is a truth that could cost Neo something more precious than his life." },
   { title: 'The Shining', rating: 'R',
-    release_date: '23-May-1980' },
+    release_date: '23-May-1980',
+    description: "Jack Torrance (Jack Nicholson) becomes winter caretaker at the isolated Overlook Hotel in Colorado, hoping to cure his writer's block. He settles in along with his wife, Wendy (Shelley Duvall), and his son, Danny (Danny Lloyd), who is plagued by psychic premonitions. As Jack's writing goes nowhere and Danny's visions become more disturbing, Jack discovers the hotel's dark secrets and begins to unravel into a homicidal maniac hell-bent on terrorizing his family." },
   { title: 'Mulholland Dr.', rating: 'R',
-    release_date: '19-Oct-2001' }
+    release_date: '19-Oct-2001',
+    description: "A dark-haired woman (Laura Elena Harring) is left amnesiac after a car crash. She wanders the streets of Los Angeles in a daze before taking refuge in an apartment. There she is discovered by Betty (Naomi Watts), a wholesome Midwestern blonde who has come to the City of Angels seeking fame as an actress. Together, the two attempt to solve the mystery of Rita's true identity. The story is set in a dream-like Los Angeles, spoilt neither by traffic jams nor smog." }
 
 ]
 
 # seed the data base
 
-Movie.destroy_all # start fresh (delete later)
+# Movie.destroy_all # start fresh (delete later)
 
 # prevent duplicated data when seeded
 more_movies.each do |movie_data|
   Movie.find_or_create_by(title: movie_data[:title]) do |movie|
     movie.rating = movie_data[:rating]
     movie.release_date = movie_data[:release_date]
+    movie.description = movie_data[:description]
   end
 end
